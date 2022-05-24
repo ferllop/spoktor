186a283747d46a41a3a5e2840ffff11b0e2b4142
Ferran Llop <contacto@ferranllop.com>
2022-05-24 23:35:24 +0200

    Make spoktor pass the playlist name to playlist generator

--- src/domain/spoktor.js
+++ src/domain/spoktor.js
@@ -5,22 +5,30 @@ import {ParserSelector} from './commands/parser-selector.js'
 export class Spoktor {
 
     constructor(inputPlaylist, outputPlaylist) {
-        this.input = Spoktor.getDigestsFor(inputPlaylist)
-        this.output = Spoktor.getDigestsFor(outputPlaylist)
+        this.input = inputPlaylist
+        this.output = outputPlaylist
     }
 
     getTraktorPlaylist(uuidGenerator) {
-        const coincidenceIndexes = this.getCoincidentDigests()
-        return new TraktorPlaylistGenerator(uuidGenerator).execute(coincidenceIndexes)
+        const playlistName = this.getPlaylistNameFrom(this.input)
+        const coincidentDigests = this.getCoincidentDigests()
+        return new TraktorPlaylistGenerator(uuidGenerator).execute(coincidentDigests, playlistName)
     }
 
     getCoincidentDigests() {
-        return new Intersect().execute(this.input, this.output)
-            .map(coincidence => this.output[coincidence])
+        const inputDigests = Spoktor.getDigestsFor(this.input)
+        const outputDigests = Spoktor.getDigestsFor(this.output)
+        return new Intersect().execute(inputDigests, outputDigests)
+            .map(coincidentDigest => outputDigests[coincidentDigest])
     }
 
     static getDigestsFor(playlist) {
         const parser = new ParserSelector().selectFor(playlist)
         return parser.parse(playlist)
+    }
+
+    getPlaylistNameFrom(playlist) {
+        const parser = new ParserSelector().selectFor(playlist)
+        return parser.extractPlaylistName(playlist)
     }
 }
