6af5e88fd4f3ec70bc9cb2e5beb6a3f8328a51e3
Ferran Llop <contacto@ferranllop.com>
2022-05-31 11:04:08 +0200

    Create a build process with rollup that bundle javascript and copy files to build directory

---
+++ rollup.config.js
@@ -0,0 +1,27 @@
+import clean from 'rollup-plugin-clean'
+import copy from 'rollup-plugin-copy'
+import {terser} from 'rollup-plugin-terser'
+
+const copyOptions = {
+    targets: [
+        {
+            src: [
+                'src/delivery/web/index.html',
+                'src/delivery/web/style.css',
+            ], dest: 'build',
+        },
+    ],
+}
+
+export default {
+    input: 'src/delivery/web/index.js',
+    output: {
+        file: './build/index.js',
+        format: 'es',
+    },
+    plugins: [
+        clean(),
+        copy(copyOptions),
+        terser(),
+    ],
+}
