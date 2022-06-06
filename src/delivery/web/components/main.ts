47082dc27359427d0bd8b25ca0b580f8817caa5a
Ferran Llop <contacto@ferranllop.com>
2022-06-06 20:56:32 +0200

    Remove commented code

--- src/delivery/web/components/main.ts
+++ src/delivery/web/components/main.ts
@@ -2,10 +2,6 @@ import {Form} from './form'
 import {Needles} from './needles'
 import {Haystack} from './haystack'
 
-export type RawPlaylistLoadEvent = CustomEventInit & {
-    detail: string
-}
-
 customElements.define('spk-needles', Needles)
 customElements.define('spk-haystack', Haystack)
 customElements.define('spk-form', Form)
@@ -18,7 +14,6 @@ template.innerHTML = `
     grid-template-columns: repeat(3, 1fr);
     place-content: center;
 }
-
 
 h1 {
     text-align: center;
@@ -34,39 +29,10 @@ h1 {
 
 export class Main extends HTMLElement {
     private shadow: ShadowRoot
-    // private needles: Digest[] = []
-    // private haystackLoadListener: EventListener
-    // private needlesLoadListener: EventListener
 
     constructor() {
         super()
         this.shadow = this.attachShadow({mode: 'open'})
         this.shadow.appendChild(template.content.cloneNode(true))
-
-        // this.haystackLoadListener = (event: CustomEventInit) => {
-        //     const haystack: Haystack | null = this.shadow.querySelector('spk-haystack')!
-        //     haystack.digests = RawPlaylist.digest(event.detail)
-        // }
-
-        // this.needlesLoadListener = (event: CustomEventInit) => {
-        //     this.needles = RawPlaylist.digest(event.detail)
-        //     const needlesEl: Needles | null = this.shadow.querySelector('spk-needles')
-        //     if(needlesEl) {
-        //         needlesEl.digests = this.needles
-        //     }
-        // }
     }
-
-    connectedCallback() {
-        // const form = this.shadow.querySelector('spk-form')
-        // form?.addEventListener('traktor-collection-load', this.haystackLoadListener)
-        // form?.addEventListener('spotify-playlist-load', this.needlesLoadListener)
-    }
-
-    disconnectedCallback() {
-        // const form = this.shadow.querySelector('spk-form')
-        // form?.removeEventListener('traktor-collection-load', this.haystackLoadListener)
-        // form?.removeEventListener('spotify-playlist-load', this.needlesLoadListener)
-    }
-
 }
