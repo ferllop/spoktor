d7b0bc604a631a986134d2eeeacfda9adbdbe23f
Ferran Llop <contacto@ferranllop.com>
2022-06-07 18:43:47 +0200

    Remove not used elements

--- src/delivery/web/components/main.ts
+++ src/delivery/web/components/main.ts
@@ -1,9 +1,7 @@
 import {Form} from './form'
-import {Needles} from './needles'
 import {DigestComponent} from './digest-component'
 import {Result} from './result'
 
-customElements.define('spk-needles', Needles)
 customElements.define('spk-result', Result)
 customElements.define('spk-form', Form)
 customElements.define('spk-digest', DigestComponent)
@@ -12,9 +10,7 @@ const template = document.createElement('template')
 template.innerHTML = `
 <style>
 :host {
-    display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    place-content: center;
+    display: flex;
 }
 
 h1 {
@@ -22,7 +18,6 @@ h1 {
     padding-bottom: 1em;
 }
 </style>
-<spk-needles></spk-needles>
 <section>
     <h1>SPOKTOR</h1>
     <spk-form></spk-form>
