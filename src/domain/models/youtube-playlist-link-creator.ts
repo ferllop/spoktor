3656fb34d45c58baa41ddace1eddfb76631d6873
Ferran Llop <contacto@ferranllop.com>
2022-06-14 23:17:23 +0200

    Test YoutubePlaylist

--- src/domain/models/youtube-playlist-link-creator.ts
+++ src/domain/models/youtube-playlist-link-creator.ts
@@ -1,11 +1,34 @@
+const regex = /youtu(?:.*\/v\/|.*v=|\.be\/|.*?embed\/)([A-Za-z0-9_\-]{11})/
+
+function pipe(value: any, ...functions: ((...param: any) => any)[]): any {
+    const [nextFunction, ...rest] = functions
+    if (nextFunction === undefined) {
+        return value
+    }
+    return pipe(nextFunction(value), ...rest)
+}
+
 export function createLink(linkList: string) {
-            const regex = /youtu(?:.*\/v\/|.*v=|\.be\/|.*?embed\/)([A-Za-z0-9_\-]{11})/
-    const links = linkList
-        .split('\n')
-        .filter( line => regex.test(line))
-        .filter( line => !line.includes('&list='))
-        .map( line => {
-            return line.match(regex)?.[1] ?? ''})
-        .join(',')
-    return 'https://www.youtube.com/watch_videos?video_ids=' + links
+    const videoIds = pipe(
+        linkList,
+        splitByLine,
+        getLinesContainingYoutubeLink,
+        getIdsFromVideoLinks)
+    return makePlaylistLinkWithVideos(...videoIds)
+}
+
+export function makePlaylistLinkWithVideos(...videos: string[]) {
+    return 'https://www.youtube.com/watch_videos?video_ids=' + videos.join(',')
+}
+
+function splitByLine(multilineText: string): string[] {
+    return multilineText.split('\n')
+}
+
+function getLinesContainingYoutubeLink(lines: string[]): string[] {
+    return lines.filter( line => regex.test(line))
+}
+
+function getIdsFromVideoLinks(links: string[]) {
+    return links.map( line => line.match(regex)?.[1] ?? '' )
 }
