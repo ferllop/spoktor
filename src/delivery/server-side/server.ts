d2dfa2c3ae720db61ec1939346dbd00c4bfa1806
Ferran Llop <contacto@ferranllop.com>
2022-06-21 17:41:34 +0200

    Move html to public directory and install htmx

--- src/delivery/server-side/server.ts
+++ src/delivery/server-side/server.ts
@@ -5,11 +5,13 @@ import {fileURLToPath} from 'url'
 const app = express()
 const port = process.env['PORT'] || 8080
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/index.html'))
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', function(_, res) {
+    res.sendFile('index.html')
 })
 app.listen(port)
 console.log('Server started at http://localhost:' + port)
