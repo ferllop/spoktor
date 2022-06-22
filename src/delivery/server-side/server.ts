f6f17d1208ce6c3303cf32ce6ca4b805d511d734
Ferran Llop <contacto@ferranllop.com>
2022-06-22 17:55:41 +0200

    Implement youtubue playlist creator into server-side delivery

--- src/delivery/server-side/server.ts
+++ src/delivery/server-side/server.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import path from 'path'
 import {fileURLToPath} from 'url'
+import {createLink} from '../../domain/models/youtube-playlist-link-creator'
 
 const app = express()
 const port = process.env['PORT'] || 8080
@@ -9,9 +10,17 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', function(_, res) {
     res.sendFile('index.html')
 })
+
+app.post('/youtube-links', (req, res) => {
+    const links = createLink(req.body['youtube-video-list'])
+    const anchor = `<a href="${links}" target="_blank" rel="noopener">Go to playlist</a>`
+    res.send(anchor)
+})
+
 app.listen(port)
 console.log('Server started at http://localhost:' + port)
